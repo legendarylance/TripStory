2072dd211c71d61373c7a4cde6f44c2bad502606
Ayazhan Zhakhan <Ayazhan Zhakhan>
2016-10-27 19:29:26 -0400

    Initial commit

---
+++ app/src/main/java/ca/utoronto/ece1778/tripstory/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.utoronto.ece1778.tripstory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
